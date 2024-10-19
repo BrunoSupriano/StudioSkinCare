97a1d0f0b0a883d0005c26f926dfb98e1687fc38
BSupriano <128910792+BrunoSupriano@users.noreply.github.com>
2024-10-19 10:52:29 -0300

    Iniciado Implementação BackEnd SpringBoot Clientes

--- src/app/ERP/Clientes/ClientForm.jsx
+++ src/app/ERP/Clientes/ClientForm.jsx
@@ -1,30 +1,61 @@
 import React from 'react';
 
-const ClienteForm = ({ handleSubmit, handleChange, cliente }) => {
-    // Garantir que cliente tenha um valor padrão
-    const defaultCliente = cliente || { nome: '', celular: '', endereco: '', cpf: '', aniversario: '' };
-
+const ClienteForm = ({ handleSubmit, handleChange, formData = {} }) => { // Adiciona um valor padrão para formData
     return (
         <form onSubmit={handleSubmit}>
             <div>
                 <label className="formlabel">Nome</label>
-                <input type="text" name="nome" value={defaultCliente.nome} onChange={handleChange} className="Custom-input" placeholder="Digite o nome" />
+                <input
+                    type="text"
+                    name="nome"
+                    value={formData.nome || ''} // Agora formData sempre será um objeto
+                    onChange={handleChange}
+                    className="Custom-input"
+                    placeholder="Digite o nome"
+                />
             </div>
             <div>
                 <label className="formlabel">Celular</label>
-                <input type="text" name="celular" value={defaultCliente.celular} onChange={handleChange} className="Custom-input" placeholder="Digite o celular" />
+                <input
+                    type="text"
+                    name="celular"
+                    value={formData.celular || ''}
+                    onChange={handleChange}
+                    className="Custom-input"
+                    placeholder="Digite o celular"
+                />
             </div>
             <div>
                 <label className="formlabel">Endereço</label>
-                <input type="text" name="endereco" value={defaultCliente.endereco} onChange={handleChange} className="Custom-input" placeholder="Digite o endereço" />
+                <input
+                    type="text"
+                    name="endereco"
+                    value={formData.endereco || ''}
+                    onChange={handleChange}
+                    className="Custom-input"
+                    placeholder="Digite o endereço"
+                />
             </div>
             <div>
                 <label className="formlabel">CPF</label>
-                <input type="text" name="cpf" value={defaultCliente.cpf} onChange={handleChange} className="Custom-input" placeholder="Digite o CPF" />
+                <input
+                    type="text"
+                    name="cpf"
+                    value={formData.cpf || ''}
+                    onChange={handleChange}
+                    className="Custom-input"
+                    placeholder="Digite o CPF"
+                />
             </div>
             <div>
                 <label className="formlabel">Aniversário</label>
-                <input type="date" name="aniversario" value={defaultCliente.aniversario} onChange={handleChange} className="Custom-input" placeholder="Digite o aniversário" />
+                <input
+                    type="date"
+                    name="aniversario"
+                    value={formData.aniversario || ''}
+                    onChange={handleChange}
+                    className="Custom-input"
+                />
             </div>
         </form>
     );
