29f37b5b93267ab34ede7da4cebd53e4514f4f76
BrunoSupriano <brunosupriano@hotmail.com>
2024-11-15 15:30:44 -0300

    Removido Gradlle (não necessário)

--- DGSBackEnd/src/main/java/com/example/dgc/DgcApplication.java
+++ DGSBackEnd/src/main/java/com/example/dgc/DgcApplication.java
@@ -3,11 +3,9 @@ package com.example.dgc;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.example.dgc")
 @EntityScan({"com.example.dgc.Clientes", "com.example.dgc.Servicos", "com.example.dgc.Usuario"})  // Certifique-se de que o pacote está correto
 
 public class DgcApplication {
