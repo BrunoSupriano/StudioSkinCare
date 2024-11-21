ad3914259545dcceab88664749b1eff5f46f805d
BSupriano <128910792+BrunoSupriano@users.noreply.github.com>
2024-11-21 20:48:56 -0300

    design e adaptações

--- src/app/ERP/Clientes/page.jsx
+++ src/app/ERP/Clientes/page.jsx
@@ -103,7 +103,7 @@ const Clientes = () => {
             {isModalOpen && (
                 <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
                     <div className="bg-white rounded-lg shadow-lg p-6 w-1/3">
-                        <h2 className="text-lg font-semibold mb-4">Cadastrar Cliente</h2>
+                        <h2 className="title text-center">Cadastrar Cliente</h2>
                         <ClienteForm handleSubmit={handleSubmit} handleChange={handleChange} formData={formData} />
                         <div className="flex justify-end mt-4">
                             <button 
